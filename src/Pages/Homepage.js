97fba2266c4fa529c8cc012c5dec19a338f87745
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2022-04-01 18:50:55 +0530

    update:css added

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div className='bg-purple-500'>
+    <h1 className='text-xl'>Hello World</h1>
+    <button>Click</button>
+  </div>
+);
+export default Home;
