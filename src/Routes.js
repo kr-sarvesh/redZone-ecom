7130a9c572200d2e4f3f81b0df0b9a56c8f4677a
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2022-04-18 20:01:55 +0530

    update:added routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Productlist';
+import Home from './Pages/Homepage';
 import Products from './Pages/Productlist';
+import MenProducts from './Pages/MenProducts';
+import WomenProducts from './Pages/WomenProducts';
+
 import Cart from './Pages/Cart';
 import Signin from './User/Signin';
 import Signup from './User/Signup';
@@ -13,6 +16,8 @@ const Routers = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Products' element={<Products />} />
+          <Route path='/MenProducts' element={<MenProducts />} />
+          <Route path='/WomenProducts' element={<WomenProducts />} />
           <Route path='/Cart' element={<Cart />} />
           <Route path='/Signin' element={<Signin />} />
           <Route path='/Signup' element={<Signup />} />
