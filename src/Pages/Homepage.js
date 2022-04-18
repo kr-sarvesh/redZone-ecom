a41d96a11e563eb95e9ac0b135c445788d9cbf00
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2022-04-18 12:17:35 +0530

    update:github

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../Hero-sec/Hero';
 import Navbar from '../Layouts/Navbar';
+
 const Home = () => (
   <div className='flex flex-col justify-between h-screen'>
     <Navbar />
--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Homepage';
+import Home from './Pages/Productlist';
 import Products from './Pages/Productlist';
 import Cart from './Pages/Cart';
 import Signin from './User/Signin';
